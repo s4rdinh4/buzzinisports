518e05a0597f39f8d4f9b6fe12f0c79ad077b144
Igor Sardinha <igor.sardinha@outlook.com>
2026-09-22 13:38:45 -0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -88,7 +88,7 @@ function SiteLogo() {
           <img
             src={buzziniLogo.url}
             alt="Buzzini Sports"
-            className={`w-auto object-contain transition-none ${!isDesktop || isDesktopScrolled ? "translate-x-1" : ""} ${isDesktopScrolled ? "h-8 sm:h-9" : "h-8 sm:h-9 md:h-24"}`}
+            className={`w-auto object-contain transition-none ${!isDesktop || isDesktopScrolled ? "translate-x-2" : ""} ${isDesktopScrolled ? "h-8 sm:h-9" : "h-8 sm:h-9 md:h-24"}`}
           />
         </a>
         <button
