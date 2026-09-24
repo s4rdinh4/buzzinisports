f1dbeb5dd730fb0b39e11b084441760cf4e9a4f0
s4rdinh4 <igoreduardosardinha@gmail.com>
2026-09-24 17:56:09 -0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -354,7 +354,7 @@ function Stories() {
   return (
     <section
       id="historias"
-      className="snap-sec relative mb-0 flex flex-col justify-start bg-background py-16 pb-24 sm:mb-12 sm:justify-center sm:py-0"
+      className="relative mb-0 flex flex-col justify-start bg-background py-16 pb-24 sm:mb-12 sm:justify-center sm:py-0"
     >
       <div className="mx-auto w-full max-w-6xl px-6 sm:px-12">
         <p className="font-mono text-xs uppercase tracking-[0.25em] text-primary">
